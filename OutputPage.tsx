e949980a20d598e6708133c0ea910fb39f959b7f
CHAABA_AYOUB <64574617+ayoub-CHAABA@users.noreply.github.com>
2023-12-17 07:24:21 +0100

    Update OutputPage.tsx

--- OutputPage.tsx
+++ OutputPage.tsx
@@ -1,21 +1,9 @@
-//Placez ce fichier dans src/routes/form/.
 import React from 'react';
-import { Typography, Button } from '@mui/material';
 
-const OutputPage = () => {
-  // Exemple de texte et de bouton de téléchargement
-  const handleDownload = () => {
-    // Logique pour gérer le téléchargement d'un fichier
-  };
-
+const OutputPage = ({ data }) => {
   return (
     <div>
-      <Typography variant="h5" gutterBottom>
-        Output Text Here
-      </Typography>
-      <Button variant="contained" onClick={handleDownload}>
-        Download
-      </Button>
+      {/* Output logic */}
     </div>
   );
 };
