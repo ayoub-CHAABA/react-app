e252bfe0388ff0a2b09970c534c3f15ffae4deb3
CHAABA_AYOUB <64574617+ayoub-CHAABA@users.noreply.github.com>
2023-12-17 07:26:14 +0100

    Update DynamicForm.tsx

--- DynamicForm.tsx
+++ DynamicForm.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 
-const DynamicForm = ({ config, onSubmit }) => {
-  // Form logic...
+// Assuming you have a type for your form fields
+interface FormField {
+  type: string;
+  name: string;
+  placeholder?: string;
+  label: string;
+}
+
+interface DynamicFormProps {
+  config: FormField[]; // Array of form field configurations
+  onSubmit: (values: any) => void; // Replace 'any' with a more specific type if possible
+}
+
+const DynamicForm: React.FC<DynamicFormProps> = ({ config, onSubmit }) => {
+  // Initialize form values
+  const initialValues = config.reduce((values, fieldConfig) => {
+    values[fieldConfig.name] = '';
+    return values;
+  }, {});
 
   return (
-    <Formik /* Formik setup */>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
       <Form>
-        {/* Form fields */}
+        {config.map((field, index) => (
+          <div key={index} className="form-field">
+            <label htmlFor={field.name}>{field.label}</label>
+            <Field name={field.name} type={field.type} placeholder={field.placeholder} />
+          </div>
+        ))}
+        <button type="submit" className="submit-button">Submit</button>
       </Form>
     </Formik>
   );
