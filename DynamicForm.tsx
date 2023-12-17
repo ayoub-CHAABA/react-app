82846eb961a1df9921f3ab1e7ab61e8ba7e100b4
CHAABA_AYOUB <64574617+ayoub-CHAABA@users.noreply.github.com>
2023-12-17 07:24:08 +0100

    Create DynamicForm.tsx

---
+++ DynamicForm.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+
+const DynamicForm = ({ config, onSubmit }) => {
+  // Form logic...
+
+  return (
+    <Formik /* Formik setup */>
+      <Form>
+        {/* Form fields */}
+      </Form>
+    </Formik>
+  );
+};
+
+export default DynamicForm;
