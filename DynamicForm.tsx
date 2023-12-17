cac51191643110a85db324dac23ebc735ede36dc
CHAABA_AYOUB <64574617+ayoub-CHAABA@users.noreply.github.com>
2023-12-17 07:41:03 +0100

    Update DynamicForm.tsx

--- DynamicForm.tsx
+++ DynamicForm.tsx
@@ -1,21 +1,53 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, useField } from 'formik';
+import { Dropdown } from 'semantic-ui-react';
 
-// Assuming you have a type for your form fields
 interface FormField {
   type: string;
   name: string;
-  placeholder?: string;
   label: string;
+  options?: { key: string; text: string; value: string }[];
 }
 
 interface DynamicFormProps {
-  config: FormField[]; // Array of form field configurations
-  onSubmit: (values: any) => void; // Replace 'any' with a more specific type if possible
+  config: FormField[];
+  onSubmit: (values: any) => void;
 }
 
+// Custom input components
+const CustomInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="form-field">
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const CustomDropdown = ({ label, ...props }) => {
+  const [field, meta, helpers] = useField(props);
+  return (
+    <div className="form-field">
+      <label>{label}</label>
+      <Dropdown
+        {...props}
+        value={field.value}
+        onChange={(_, data) => helpers.setValue(data.value)}
+        onBlur={() => helpers.setTouched(true)}
+      />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+// DynamicForm component
 const DynamicForm: React.FC<DynamicFormProps> = ({ config, onSubmit }) => {
-  // Initialize form values
   const initialValues = config.reduce((values, fieldConfig) => {
     values[fieldConfig.name] = '';
     return values;
@@ -23,15 +55,23 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ config, onSubmit }) => {
 
   return (
     <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      <Form>
-        {config.map((field, index) => (
-          <div key={index} className="form-field">
-            <label htmlFor={field.name}>{field.label}</label>
-            <Field name={field.name} type={field.type} placeholder={field.placeholder} />
-          </div>
-        ))}
-        <button type="submit" className="submit-button">Submit</button>
-      </Form>
+      {formik => (
+        <Form>
+          {config.map((field, index) => {
+            switch (field.type) {
+              case 'text':
+                return <CustomInput key={index} {...field} />;
+              case 'dropdown':
+                return <CustomDropdown key={index} {...field} />;
+              case 'date':
+                return <CustomInput key={index} type="date" {...field} />;
+              default:
+                return null;
+            }
+          })}
+          <button type="submit" className="submit-button">Submit</button>
+        </Form>
+      )}
     </Formik>
   );
 };
