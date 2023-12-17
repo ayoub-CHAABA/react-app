c9c14454f8356a49b277bfcfdff125016fe1b4b3
CHAABA_AYOUB <64574617+ayoub-CHAABA@users.noreply.github.com>
2023-12-17 07:38:44 +0100

    Update formConfigs.ts

--- formConfigs.ts
+++ formConfigs.ts
@@ -1,4 +1,36 @@
 // formAConfig.ts
-export const formAConfig = [
-  // Form A configuration...
+
+interface FormField {
+  type: string;
+  name: string;
+  label: string;
+  options?: { key: string; text: string; value: string }[];
+}
+
+export const formAConfig: FormField[] = [
+  {
+    type: 'text',
+    name: 'indexName',
+    label: 'Index Name',
+  },
+  {
+    type: 'dropdown',
+    name: 'scope',
+    label: 'Scope',
+    options: [
+      { key: 'option1', text: 'Option 1', value: 'option1' },
+      { key: 'option2', text: 'Option 2', value: 'option2' },
+      // Add more options as needed
+    ],
+  },
+  {
+    type: 'date',
+    name: 'startDate',
+    label: 'Start Date',
+  },
+  {
+    type: 'date',
+    name: 'endDate',
+    label: 'End Date',
+  },
 ];
