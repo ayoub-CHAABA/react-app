ebbf42ccfa1811ab9bf3d269919ed557999347cc
CHAABA_AYOUB <64574617+ayoub-CHAABA@users.noreply.github.com>
2023-12-17 07:23:38 +0100

    Update Sidebar.tsx

--- Sidebar.tsx
+++ Sidebar.tsx
@@ -1,36 +1,14 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import menuItems from '../../menu.json'; // Assurez-vous que le chemin d'accès est correct
+import { Menu } from 'semantic-ui-react';
 
-const drawerWidth = 240;
+const SidebarMenu = () => {
+  // Sidebar logic...
 
-const Sidebar = () => {
   return (
-    <Drawer
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
-          width: drawerWidth,
-          boxSizing: 'border-box',
-        },
-      }}
-      variant="permanent"
-      anchor="left"
-    >
-      <List>
-        {menuItems.map((item, index) => (
-          <ListItem button key={index} component={NavLink} to={item.link} activeClassName="Mui-selected" exact>
-            <ListItemText primary={item.title} />
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
+    <Menu vertical>
+      {/* Menu items... */}
+    </Menu>
   );
 };
 
-export default Sidebar;
+export default SidebarMenu;
