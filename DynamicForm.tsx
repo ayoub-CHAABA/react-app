27aff1db94c2f77e7a52a3332c578954d172df24
CHAABA_AYOUB <64574617+ayoub-CHAABA@users.noreply.github.com>
2023-12-17 07:53:46 +0100

    Update DynamicForm.tsx

--- DynamicForm.tsx
+++ DynamicForm.tsx
@@ -1,6 +1,10 @@
+npm install react-dropzone
+
+npm install react-dropzone semantic-ui-react
 import React from 'react';
 import { Formik, Form, Field, useField } from 'formik';
 import { Dropdown } from 'semantic-ui-react';
+import { DropzoneArea } from 'react-dropzone';
 
 interface FormField {
   type: string;
@@ -14,16 +18,13 @@ interface DynamicFormProps {
   onSubmit: (values: any) => void;
 }
 
-// Custom input components
 const CustomInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
     <div className="form-field">
       <label htmlFor={props.id || props.name}>{label}</label>
       <input {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
+      {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
     </div>
   );
 };
@@ -39,39 +40,62 @@ const CustomDropdown = ({ label, ...props }) => {
         onChange={(_, data) => helpers.setValue(data.value)}
         onBlur={() => helpers.setTouched(true)}
       />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
+      {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
     </div>
   );
 };
 
-// DynamicForm component
+const CustomCheckbox = ({ label, ...props }) => {
+  const [field, meta] = useField({ ...props, type: 'checkbox' });
+  return (
+    <div className="form-field">
+      <label className="checkbox-label">
+        <input {...field} type="checkbox" />
+        {label}
+      </label>
+      {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
+    </div>
+  );
+};
+
+const CustomFileUpload = ({ label, ...props }) => {
+  const [field, , helpers] = useField(props);
+  return (
+    <div className="form-field">
+      <label>{label}</label>
+      <DropzoneArea
+        {...props}
+        onDrop={(files) => helpers.setValue(files)}
+      />
+    </div>
+  );
+};
+
 const DynamicForm: React.FC<DynamicFormProps> = ({ config, onSubmit }) => {
   const initialValues = config.reduce((values, fieldConfig) => {
-    values[fieldConfig.name] = '';
+    values[fieldConfig.name] = fieldConfig.type === 'checkbox' ? false : '';
     return values;
   }, {});
 
   return (
     <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      {formik => (
-        <Form>
-          {config.map((field, index) => {
-            switch (field.type) {
-              case 'text':
-                return <CustomInput key={index} {...field} />;
-              case 'dropdown':
-                return <CustomDropdown key={index} {...field} />;
-              case 'date':
-                return <CustomInput key={index} type="date" {...field} />;
-              default:
-                return null;
-            }
-          })}
-          <button type="submit" className="submit-button">Submit</button>
-        </Form>
-      )}
+      <Form>
+        {config.map((field, index) => {
+          switch (field.type) {
+            case 'text':
+              return <CustomInput key={index} {...field} />;
+            case 'dropdown':
+              return <CustomDropdown key={index} {...field} />;
+            case 'boolean':
+              return <CustomCheckbox key={index} {...field} />;
+            case 'file':
+              return <CustomFileUpload key={index} {...field} />;
+            default:
+              return null;
+          }
+        })}
+        <button type="submit" className="submit-button">Submit</button>
+      </Form>
     </Formik>
   );
 };
